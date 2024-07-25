4b6a3b04e0a6e3f7487e2f224780ceeb0bd2906c
Kytyzovns <kytyzovns@gmail.com>
2024-07-25 22:12:56 +0300

    pete-the-baker.js

--- testing.js
+++ testing.js
@@ -5,21 +5,21 @@ function cakes(recipe, available) {
     for(let key in recipe) {
         for(let key1 in available) {
             if(key === key1) {
-                res.push(Math.floor(available.key1 / recipe.key));
+                res.push(Math.floor(available[key1] / recipe[key]));
                 test = false;
                 break;
             }
-            if (test) {
-                res = [0];
-                break;
-            }
+        }
+        if (test) {
+            res = [0];
+            break;
         }
     }
-
+ console.log(res);
     res1 = res[0];
 
     res.forEach((curr, index, arr) => {
-        if(curr > 0 && curr < arr[index+1]) {
+        if(curr > 0 && curr < arr[index+1] && res1 !==0) {
             res1 = curr;
         } else if(curr === 0) {
             res1 = 0;
@@ -28,4 +28,4 @@ function cakes(recipe, available) {
     return res1;
 }
 
-console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
+console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}))
