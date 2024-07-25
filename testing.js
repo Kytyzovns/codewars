fd03e94c0e6dda4d641df2f4e5f33f041fa5a8d8
Kytyzovns <kytyzovns@gmail.com>
2024-07-25 18:56:44 +0300

    coding

--- testing.js
+++ testing.js
@@ -1,22 +1,31 @@
-function dirReduc(arr) {
-    const check2 = () => {
-        let i = 0;
-        let j = false;
-
-        for (i =0; i < arr.length; i++) {
-            if (arr[i] + arr[i + 1] === "NORTHSOUTH" || arr[i] + arr[i + 1] === "SOUTHNORTH" || arr[i] + arr[i + 1] === "WESTEAST" || arr[i] + arr[i + 1] === "EASTWEST") {
-                arr.splice(i, 2);
-                j = true;
+function cakes(recipe, available) {
+    let res = [];
+    let res1 = 0;
+    let test = true;
+    for(let key in recipe) {
+        for(let key1 in available) {
+            if(key === key1) {
+                res.push(Math.floor(available.key1 / recipe.key));
+                test = false;
+                break;
+            }
+            if (test) {
+                res = [0];
                 break;
             }
         }
-
-        if (j) {
-            check2();
-        }
     }
-    check2();
-    return arr;
+
+    res1 = res[0];
+
+    res.forEach((curr, index, arr) => {
+        if(curr > 0 && curr < arr[index+1]) {
+            res1 = curr;
+        } else if(curr === 0) {
+            res1 = 0;
+        }
+    })
+    return res1;
 }
 
-console.log(dirReduc(["EAST", "EAST", "WEST", "NORTH", "WEST", "EAST", "EAST", "SOUTH", "NORTH", "WEST"]));
+console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
