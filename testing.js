f2734db9f41c3190ff4fdbc4ff34b0f8c0296fa5
Kytyzovns <kytyzovns@gmail.com>
2024-07-26 22:11:55 +0300

    coding

--- testing.js
+++ testing.js
@@ -1,31 +1,8 @@
-function cakes(recipe, available) {
-    let res = [];
-    let res1 = 0;
-    let test = true;
-    for(let key in recipe) {
-        for(let key1 in available) {
-            if(key === key1) {
-                res.push(Math.floor(available[key1] / recipe[key]));
-                test = false;
-                break;
-            }
-        }
-        if (test) {
-            res = [0];
-            break;
-        }
-    }
- console.log(res);
-    res1 = res[0];
+const kok = [
+    {kek: 1},
+    {kek2: 2},
+    {kek3: 3}
+]
 
-    res.forEach((curr, index, arr) => {
-        if(curr > 0 && curr < arr[index+1] && res1 !==0) {
-            res1 = curr;
-        } else if(curr === 0) {
-            res1 = 0;
-        }
-    })
-    return res1;
-}
-
-console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}))
+let kekkok = [...kok]
+console.log(kekkok);
