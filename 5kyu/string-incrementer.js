3efae9374a9eb374150b39c1d607feb3de80139f
Kytyzovns <kytyzovns@gmail.com>
2024-08-01 22:44:03 +0300

    string-incrementer.js

--- 5kyu/string-incrementer.js
+++ 5kyu/string-incrementer.js
@@ -1,3 +1,22 @@
+//Problem: Your job is to write a function which increments a string, to create a new string.
+//
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples:
+//
+// foo -> foo1
+//
+// foobar23 -> foobar24
+//
+// foo0042 -> foo0043
+//
+// foo9 -> foo10
+//
+// foo099 -> foo100
+//
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+//Solution:
 function incrementString (strng) {
     let word = "";
     let num = "";
@@ -15,7 +34,7 @@ function incrementString (strng) {
     console.log(word);
     console.log(num);
     if(num === "") {
-       return word + 1;
+        return word + 1;
     } else if(num.length > num1.toString().length) {
         for(let i = 0; i < num.length - num1.toString().length; i++) {
             word += 0;
